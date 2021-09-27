1abc7979905c3b27e77057d7ebe444cefcf69065
martinalmeida <64678166+martinalmeida@users.noreply.github.com>
2021-09-27 01:06:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
   <link rel="shortcut icon" href="assets/img/icons/icon-48x48.png" />
-  <title></title>
+  <title>ARROCERA</title>
 </head>
 
 <body style="background-color: #222E3C;">
